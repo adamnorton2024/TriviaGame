77951da9bcd31f999f859018437f7c55f638300a
Adam Norton <adam@adamnorton.com>
2019-09-23 15:53:51 -0500

    just setting up basic layout.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+var questions = [{q: "what?", ca: "yes", ia1: "no", ia2: "probably", ia3: "what?"}, {q: "how?", ca: "nope", ia1: "who cares", ia2: "wft?", ia3: "omg?"}, {q: "when?", ca: "true", ia1: "false"}];
+
+var correct = 0;
+var incorrect = 0;
+
+
+});
+
