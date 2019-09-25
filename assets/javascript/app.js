1f56b91d6f5a8a112dd7f93a716db95dad7088c3
Adam Norton <adam@adamnorton.com>
2019-09-25 00:39:42 -0500

    Fixed spelling in logo, added reset button, just working on fixing a few remaining bugs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,14 @@
 
 //define global variables
-var questions = [{ q: "What fruit is a cross between a blackberry and a rasberry?", ca: "Tayberry", ia1: "Mango", ia2: "Grape", ia3: "Blueberry" }, { q: "What is fear of the color purple know as?", ca: "Porphyrophobia", ia1: "Acrophobia", ia2: "Arachniphobia", ia3: "Claustrophobia" }, { q: "What is another name for a prarie wolf?", ca: "Coyote", ia1: "Hyena", ia2: "Jackel", ia3: "Anubis" }, { q: "Which is the largest whale?", ca: "Blue Whale", ia1: "Humpback Whale", ia2: "Minke Whale", ia3: "Moby Dick" }, { q: "What is the only mammal with 4 forward facing knees?", ca: "Elephant", ia1: "Hippo", ia2: "Rhino", is3: "Dog" }, { q: "When standing in the sunlight, what vitamin does our body synthesize?", ca: "Vitamin D", ia1: "Vitamin C", ia2: "Vitamin B", ia3: "Calcium" }, { q: "What planet is named after the God of Love?", ca: "Venus", ia1: "Aphrodite", ia2: "Mercury", ia3: "Saturn" }, { q: "What gas is exhaled by humans?", ca: "Carbon Dioxide", ia1: "Methane", ia2: "Helium", ia3: "Oxygen" }, { q: "What is the heaviest internal organ in the human body?", ca: "Liver", ia1: "Heart", ia2: "Spleen", ia3: "Gallbladder" }, { q: "What is added to bread to make it swell?", ca: "Yeast", ia1: "Almond", ia2: "Cashew", ia3: "Tomato" }];
+var questions = [{ q: "What fruit is a cross between a blackberry and a rasberry?", ca: "Tayberry", ia1: "Mango", ia2: "Grape", ia3: "Blueberry" }, { q: "What is fear of the color purple know as?", ca: "Porphyrophobia", ia1: "Acrophobia", ia2: "Arachniphobia", ia3: "Claustrophobia" }, { q: "What is another name for a prairie wolf?", ca: "Coyote", ia1: "Hyena", ia2: "Jackel", ia3: "Anubis" }, { q: "Which is the largest whale?", ca: "Blue Whale", ia1: "Humpback Whale", ia2: "Minke Whale", ia3: "Moby Dick" }, { q: "What is the only mammal with 4 forward facing knees?", ca: "Elephant", ia1: "Hippo", ia2: "Rhino", is3: "Dog" }, { q: "When standing in the sunlight, what vitamin does our body synthesize?", ca: "Vitamin D", ia1: "Vitamin C", ia2: "Vitamin B", ia3: "Calcium" }, { q: "What planet is named after the God of Love?", ca: "Venus", ia1: "Aphrodite", ia2: "Mercury", ia3: "Saturn" }, { q: "What gas is exhaled by humans?", ca: "Carbon Dioxide", ia1: "Methane", ia2: "Helium", ia3: "Oxygen" }, { q: "What is the heaviest internal organ in the human body?", ca: "Liver", ia1: "Heart", ia2: "Spleen", ia3: "Gallbladder" }, { q: "What is added to bread to make it swell?", ca: "Yeast", ia1: "Almond", ia2: "Cashew", ia3: "Tomato" }];
 
 var correct = 0;
 var incorrect = 0;
+var unanswered = 0;
 
-var resetResultTimer = 4;
+var buttonClicked = true;
+
+var resetResultTimer = 3;
 var resetQuestionTimer = 11;
 var timer;
 var intervalId;
@@ -27,37 +30,40 @@ $(document).ready(function() {
  });
 
  function chooseAnswer(){
-     $('.answer').on("click", checkAnswer);
+     if(!buttonClicked){
+         buttonClicked = true;
+         console.log("button clicked set to true");
+         console.log("button clicked");
+         $('.answer').on("click", checkAnswer);
+     };
+     
  };
 
 function checkAnswer() {
     stopTimer();
-    if ($(this).text() === questions[questionNumber].ca) {
-        console.log("Correct!");
-        correct++;
-        $(this).addClass("correct-answer");
-        $('#timer').text("Correct!");
-    } else {
-        console.log("Wrong");
-        incorrect++;
-        $(this).addClass("wrong-answer");
-        $('#timer').text("Correct Answer: " + questions[questionNumber].ca);
+    if(buttonClicked){
+        if ($(this).text() === questions[questionNumber].ca) {
+            console.log("Correct!");
+            correct++;
+            console.log(correct);
+            $(this).addClass("correct-answer");
+            $('#timer').text("Correct!");
+        } else {
+            console.log("Wrong");
+            incorrect++;
+            console.log(incorrect);
+            $(this).addClass("wrong-answer");
+            $('#timer').text("Correct Answer: " + questions[questionNumber].ca);
 
+        }
     }
-
     startResultTimer();
 
-    
 };
-
-//  function startGame(){
-
-//     $('#question-div').text(questions[questionNumber].q);
-
-//  };
 
  function nextQuestion() {
 
+     $('#timer').empty();
     // print current answer to HTML element
     $('#question-div').text(questions[questionNumber].q);
 
@@ -78,12 +84,14 @@ function checkAnswer() {
 
      //print answers to HTML elements
      for (var j = 0; j < randomizedAnswers.length; j++){
-         console.log("j = " + j);
+         //console.log("j = " + j);
          $("#text-answer-" + j).text(randomizedAnswers[j]);
-         console.log(randomizedAnswers[j]);
+         //console.log(randomizedAnswers[j]);
      }
 
      startQuestionTimer();
+     buttonClicked = false;
+     console.log("button clicked back to false");
      chooseAnswer();
  };
 
@@ -143,12 +151,26 @@ function countResult() {
         if(questionNumber < 10){
             nextQuestion();
         } else {
-            console.log("Game Over!");
+            gameOver();
         }
     }
 };
 
+function gameOver(){
+    console.log("Game Over");
+    $('#timer').empty();
+    $("#question-div").empty();
+    $(".answer").css("display", "none");
+    $("#answers-div").append("<p>Correct Answers: " + correct + "</p>");
+    $("#answers-div").append("<p>Incorrect Answers: " + incorrect + "</p>");
+    $("#answers-div").append("<p>Unanswered: " + unanswered + "</p>");
+    $("#answers-div").append("<div class='text answer' id='btn-reset'>Reset</div>");
+    
+
+};
+
 function outOfTime(){
-    $("#timer").addClass("times-up");
-    $('#timer').text("Times Up!");
-}
+    unanswered++;
+    $('#timer').text("Correct Answer: " + questions[questionNumber].ca);
+    startResultTimer();
+};
